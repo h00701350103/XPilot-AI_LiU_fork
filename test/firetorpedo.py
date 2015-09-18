0d16aae8f1440caab151419870b148b67412d75c
Tommy Persson <tommy.persson@liu.se>
2015-09-18 14:53:48 +0200

    Adding firetorpedo test script.

--- test/firetorpedo.py
+++ test/firetorpedo.py
@@ -1,6 +1,14 @@
 import libpyAI as ai
 
+count = 0
+
 def AI_loop():
-    ai.fireTorpedo()
+    global count
+    count += 1
+    if count % 10 == 0:
+        print ("SHIELD", ai.selfShield())
+        if ai.selfShield():
+            ai.shield()
+        ai.fireTorpedo()
 
 ai.start(AI_loop,["-name","FireTorpedo", "-join"])
